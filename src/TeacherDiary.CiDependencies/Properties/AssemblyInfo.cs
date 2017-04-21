cfeb23b4d7336a0b82974cddaa570c8b53d07e0c
krastev-pc <andriyan.krastev@gmail.com>
2017-04-22 00:48:28 +0300

    CI integration

--- src/TeacherDiary.CiDependencies/Properties/AssemblyInfo.cs
+++ src/TeacherDiary.CiDependencies/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("eb83402b-2ad7-49bc-9515-170238901714")]
+[assembly: Guid("455282a1-00d7-42bd-93c8-ee1a754704e2")]
 
 // Version information for an assembly consists of the following four values:
 //
