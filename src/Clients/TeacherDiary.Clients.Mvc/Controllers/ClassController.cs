617c32b52bb9d23815e8f56e008016499c37b386
krastev-pc <andriyan.krastev@gmail.com>
2017-04-23 01:24:03 +0300

    added automapper

---
+++ src/Clients/TeacherDiary.Clients.Mvc/Controllers/ClassController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using TeacherDiary.Clients.Mvc.ViewModels.Class;
+
+namespace TeacherDiary.Clients.Mvc.Controllers
+{
+    public class ClassController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ClassViewModel model)
+        {
+            if (!ModelState.IsValid || model == null)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/Clients/TeacherDiary.Clients.Mvc/ViewModels/Class/ClassViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeacherDiary.Clients.Mvc.ViewModels.Class
+{
+    public class ClassViewModel
+    {
+    }
+}
