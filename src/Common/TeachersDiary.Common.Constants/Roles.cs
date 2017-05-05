5a3b35443e097f8ec239002c7d476e6a1f295501
Krastev Laptop <andriyan.krastev@gmail.com>
2017-05-05 15:40:38 +0300

    added roles

---
+++ src/Common/TeachersDiary.Common.Constants/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace TeachersDiary.Common.Constants
+{
+    public class Roles
+    {
+        public const string Student = "Student";
+
+        public const string Teacher = "Teacher";
+
+        public const string SchoolAdministrator = "SchoolAdministrator";
+
+        public const string Administrator = "Administrator";
+    }
+}
--- src/Data/TeachersDiary.Data.Ef/Migrations/Configuration.cs
+++ src/Data/TeachersDiary.Data.Ef/Migrations/Configuration.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
 using System.Data.Entity.Migrations;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TeachersDiary.Common.Constants;
+using TeachersDiary.Data.Ef.Entities;
 
 namespace TeachersDiary.Data.Ef.Migrations
 {
@@ -8,6 +14,34 @@ namespace TeachersDiary.Data.Ef.Migrations
         {
             AutomaticMigrationsEnabled = false;
             AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(TeachersDiaryDbContext context)
+        {
+            CreateRoles(context);
+        }
+
+        private void CreateRoles(TeachersDiaryDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                var roles = new List<string>
+                {
+                    Roles.Student,
+                    Roles.Teacher,
+                    Roles.SchoolAdministrator,
+                    Roles.Administrator
+                };
+
+                foreach (var roleName in roles)
+                {
+                    var roleStore = new RoleStore<IdentityRole>(context);
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    var role = new IdentityRole { Name = roleName };
+
+                    roleManager.Create(role);
+                }
+            }
         }
     }
 }
